88fab53fe0f26ec109371195ed47356e7a444a24
Antonio Esposito <antonio.esposito@vodafone.com>
2022-10-11 10:46:48 +0200

    creato il repository

---
+++ checkiosxr.py
@@ -0,0 +1,105 @@
+"""
+L'obbiettivo di questo programma è raccogliere le informazioni da un cisco IOSXR per verificare il ripristino dei servizi dopo la procedura di upgrade.
+
+il programma richiede di passare un argomento pre o post per:
+  - se si sceglier pre allora vengono raccolti i dati e salvati in un file precheck.json.
+  - se si sceglie post allora vengono raccolti i dati e salvati in un file postcheck.json e viene poi attivata un procedura per confrontare i due file precheck.jsoon e 
+    postcheck.jsono
+
+Le informazioni vengono raccolte dal device usando netconf quindi usiamo ncclient. il codice per fare la richiesta netconf al devide è racchiuso nel module myprivatelibrary nella funzione
+netconfrequest che prende in ingresso due parametri:
+ - Una stringa contenete il filtro xml da applicare alla richista 
+ - device a cui applicare la richiesta. Nota che device è un oggetto che viene creato con ncclient
+
+Nota che i filtri xml da applicare alla richiesta sono salvati nella cartella templatexml e vengono letti in sequenza grazie alla libreria os, L'ordine con cui vengono letti questi file 
+è per data per cui il primo ad essere creato e anche il primo della lista e cosi via.
+
+
+le seguenti informazioni sulle intrfacce e sui protocolli, abbiamo cercato di mantenere una corrispondenza tra le richieste netconf che facciamo al device e i comandi utlizzati 
+sul device per visualizzare le stesse informazioni, questo perchè le persone del nostro gruppo sono sicuramente più abituate a ragionare in termini di comandi cli piuttoso che modelli
+di YANG. Le seguenti informazioni verranno raccolte
+- show isis neighbors
+- show bgp vpnv4 unicast summary
+- show bgp vpnv6 unicast summary
+- show bundle bunde-eth 
+
+"""
+
+import xmltodict
+import json
+import os
+from argparse import ArgumentParser
+
+from ncclient import manager
+
+from myprivatelibrary import netconfrequest
+
+parser = ArgumentParser(description='Usage:')
+
+# Script argument for pre e post
+
+
+
+parser.add_argument('-t', '--preorpost', type=str, required=True)
+
+args = parser.parse_args()
+
+"""
+# script arguments for manager
+    parser.add_argument('-a', '--host', type=str, required=True,
+                        help="Device IP address or Hostname")
+    parser.add_argument('-u', '--username', type=str, required=True,
+                        help="Device Username (netconf agent username)")
+    parser.add_argument('-p', '--password', type=str, required=True,
+                        help="Device Password (netconf agent password)")
+    parser.add_argument('--port', type=int, default=830,
+                        help="Netconf agent port")
+    args = parser.parse_args()
+"""
+
+
+
+device = manager.connect(host='v-mivpe015', port=830, username='EspositoA1', password='admin', hostkey_verify=False, device_params={}, allow_agent=False, look_for_keys=False)
+
+D = dict()
+
+path = 'templatexml'
+for filename in os.listdir(path):
+    if filename.endswith('.xml'):
+            with open(os.path.join(path, filename), 'r') as g:
+                D[filename[:-4]] = netconfrequest(g.read(), device)
+
+
+
+if args.preorpost == 'pre':
+    json.dump(D, fp = open('precheck.json', 'w'), indent = 4)
+elif args.preorpost == 'post':
+    json.dump(D, fp = open('postcheck.json', 'w'), indent = 4)
+
+    postcheck = json.load(open('postcheck.json'))
+    Postcheck = json.load(open('postcheck.json'))
+
+    print(postcheck.keys())
+    print(Postcheck.keys())
+
+    if postcheck == Postcheck:
+        print('i due file json sono identici')
+    else:
+        if postcheck['showisisnei'] == Postcheck['showisisnei']:
+          print('ISIS OK')
+        else:
+            print('ISIS KO')
+        if postcheck['showbgpvpv4unicastsummary'] == Postcheck['showbgpvpv4unicastsummary']:
+          print('BGP VPNV4 OK')
+        else:
+            print('BGP VPNV4 KO')
+        if postcheck['showbgpvpv6unicastsummary'] == Postcheck['showbgpvpv6unicastsummary']:
+          print('BGP VPNV6 OK')
+        else:
+            print('BGP VPNV6 KO')
+        if postcheck['showbundlebundleethernet'] == Postcheck['showbundlebundleethernet']:
+          print('BUNDLE ETHERNET OK')
+        else:
+            print('BUNDLE ETHERNET KO')
+else:
+    print('Devi scegliere pre o post')
---
+++ myprivatelibrary.py
@@ -0,0 +1,27 @@
+import xmltodict
+import json
+import os
+
+from ncclient import manager
+
+"""
+Questa funzione prende in ingresso due parametri:
+ - Una stringa contenete il filtro xml da applicare alla richista
+ - device a cui applicare la richiesta. Nota che device è un oggetto che viene creato con ncclient
+
+"""
+
+def netconfrequest(subtree_filter, device):
+
+    #crea una string str_nc_get_reply contenete la risposta xml del device
+    str_nc_get_reply = device.get(('subtree', subtree_filter)).data_xml
+    
+    #Crea un file di testo check.xml contenente la stringa con la risposta xml
+    with open("%s.xml" % 'check', 'w') as f:
+        f.write(str_nc_get_reply)
+        f.close()
+    
+    #Converte xml in un dizionatio ordinato, una speciale classe di dizionario che non permette agli elementi di cambiare ordine, Questo serve perché nei file xml l'ordine è importante.
+     
+    with open('check.xml') as fxml:
+        return xmltodict.parse(fxml.read())
